d23c0dfd5d30d99dc7c36a91341f41632be60cdb
AliNaqvi5 <alimir579@gmail.com>
2023-08-31 16:07:20 +0500

    Moisture sensor

--- DHTsensor.ino
+++ DHTsensor.ino
@@ -1,15 +1,20 @@
 #include <dht.h>
+#include <LiquidCrystal.h>
 
 dht DHT;
 
-#define DHT11_PIN 7
+#define DHT11_PIN 2
 #define Rres 5 // Kilo Ohms
 #define Vref 5
 // #define slope -1.404966547
 #define A 12518931.7
 
 
+  LiquidCrystal lcd(8,9,10, 11,12,13);  //RS,En D4,D5,D6,D7
+
 int LDRPin = A0;      // input pin for the potentiometer
+int sensor_pin = A1; // input pin for soil moisture sensor
+
 float digitalValue = 0;  // variable to store the value coming from the sensor
 float Vres = 0.00;
 float Vldr = 0.00;
@@ -21,6 +26,7 @@ double lux = 0.0000;
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2);
 }
 
 void loop() {
@@ -61,9 +67,34 @@ void loop() {
       //A*R^B
       lux = pow(Rldr, slope) * A;
       //  Serial.println(A);
-      Serial.println(lux);
+      // Serial.println(lux);
+
+// Soil Moisture sensor 
+
+  float moisture_percentage;
+  int sensor_analog;
+  sensor_analog = analogRead(sensor_pin);
+  moisture_percentage = ( 100 - ( (sensor_analog/1023.00) * 100 ) );
+  // Serial.print("Moisture Percentage = ");
+  // Serial.print(sensor_analog);
+  // Serial.print("%\n\n");
+
+lcd.print("   CIRCUIT DIGEST");//print name
 
 
+lcd.setCursor(0, 2); // set the cursor to column 0, line 2
+
+
+lcd.print("TEMP ");//print name
+lcd.print(DHT.temperature);
+
+delay(750);//delay of 0.75sec
+
+
+lcd.scrollDisplayLeft();//shifting data on LCD
+
+
+lcd.setCursor(0, 0);// set the cursor to column 0, line1
 
   delay(1000);
 }
